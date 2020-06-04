70ec94e804718fc9fa10c61d75537b8344faf7a7
antigones <>
2020-06-04 13:39:16 +0200

    magicleon suggestion accepted

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,14 +11,14 @@ const RESOURCES = {
 "assets/assets/Pacifico-Regular.ttf": "9b94499ccea3bd82b24cb210733c4b5e",
 "assets/FontManifest.json": "a588f11588c15e5d69c762ee7671a585",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "948fc05cfc434fb624a1a599cd9fb21e",
+"assets/LICENSE": "f055eab0ac9d51d3df871145e910ca73",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "4daebebff15d7fb0302b954bc22736f6",
 "icons/Icon-192.png": "4d24f664e9b52c4fc3cf4b62a82bf8ba",
 "icons/Icon-512.png": "e6c2c2034d2223ee4b17e4d05da5c193",
 "index.html": "2e8285eb99afc369c41d60a1cc302008",
 "/": "2e8285eb99afc369c41d60a1cc302008",
-"main.dart.js": "ccc9bd6b9f07ab6c016c2d68666ed1de",
+"main.dart.js": "a28779254c4023d3b100cc6b907c3525",
 "manifest.json": "7af2fee3bcf987f44ff84b827d37d923"
 };
 
